2e63d9d356f956ffaf72cc8873d471ba8a2e5adf
pnw <patrick@levr.com>
2013-12-08 15:21:13 +1300

    bug fixes on function to update sid data with osm ids

--- app.py
+++ app.py
@@ -30,8 +30,9 @@ def get_all_intersections():
         nodes = red.keys('iid:*')
         feed = itertools.imap(red.hgetall, nodes)
         dehydrated = itertools.imap(dehydrate, feed)
+        filtered = filter(lambda obj: obj.get('osm_id') and obj.get('avg_cong'), dehydrated)
         return json.dumps(dict(
-            objects=list(dehydrated)
+            objects=list(filtered)
         ))
     except Exception as e:
         logger.exception(e)
@@ -46,28 +47,28 @@ def dehydrate(node):
 
 @app.route('/update_osm')
 def update_osm():
-    return update()
+    try:
+        return update()
+    except Exception as e:
+        return e
 
 def update():
-    try:
-        with open('export.geojson', 'w') as f:
-            features = f['features']
-            for feat in features:
-                osm_id = feat['id'].split('/')[1]
-                sid = feat['props']['chchsid']
-                lon, lat = feat['geometry']['coordinates']
+    with open('export.geojson', 'r') as f:
+        data = json.load(f)
+        features = data['features']
+        for feat in features:
+            osm_id = feat['id'].split('/')[1]
+            sid = feat['properties']['chchsid']
+            lon, lat = feat['geometry']['coordinates']
 
-                assign(sid, lat=lat, lon=lon, osm_id=osm_id)
-    except Exception as e:
-        logger.exception(e)
+            assign(sid, lat=lat, lon=lon, osm_id=osm_id)
 
 def assign(sid, **mapping):
     sid_key = 'sid:'+sid
     iid_key = red.get('sid:'+sid)
     if not iid_key:
-        iid_key = 'iid:' + uuid.uuid4()
+        iid_key = 'iid:' + str(uuid.uuid4())
         red.set(sid_key, iid_key)
-
     mapping.update(dict(sid=sid))
     red.hmset(iid_key, mapping)
 
